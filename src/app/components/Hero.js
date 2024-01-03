35a21b0ce3129dd4d59367c4b681431bc14d5d1f
Reka <reka.kovacs1993@gmail.com>
2024-01-03 18:47:06 +0000

    animation

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -1,10 +1,29 @@
 import Image from "next/image";
+import gsap from "gsap";
 
 export default function Hero() {
+
+    gsap.from("p", {
+        opacity: 0,
+        x: -20,
+        ease: Expo.easeInOut
+    })
+
+    // gsap.staggerFrom("nav ul li", 1, {
+    //     opacity: 0,
+    //     x: -20,
+    //     ease: Power3.easeInOut
+    // }, 0.08)
+
+
     return (
-        <div className="w-full mt-10">
+        <div className="w-full">
             {/* Computer Layout */}
-            <div className="hidden md:block items-center">
+            <div className="hidden md:flex items-center justify-center">
+
+                    <div id="line-one"></div>
+                    <div id="line-two"></div>
+
                 <div className='flex text-7xl mt-40  items-center'>
                     <p className=' hover:text-9xl'>C</p>
                     <p className=' hover:text-9xl'>S</p>
@@ -15,7 +34,7 @@ export default function Hero() {
                 </div>
             </div>
             {/* Mobile and Tablet Layout */}
-            <div className="h-screen relative md:hidden flex items-center">
+            <div className="h-screen md:hidden flex items-center">
             <div className='flex text-7xl mt-40'>
                     <p className=' hover:text-9xl'>C</p>
                     <p className=' hover:text-9xl'>S</p>
