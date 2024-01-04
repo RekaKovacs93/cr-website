21174678d82ee31686ace732b1501517310d2798
Reka <reka.kovacs1993@gmail.com>
2024-01-04 11:36:40 +0000

    logo anim

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -1,12 +1,20 @@
 'use client'
 
+import Image from 'next/image';
 import gsap from 'gsap';
 import { useEffect } from 'react';
 
 export default function Hero() {
 
     useEffect(() => {
-        gsap.to(".logo", { opacity: 1, duration: 0.5 });
+        const logotl = gsap.timeline();
+        logotl.to(".letters-1", { opacity: 1, duration: 0.5 });
+        logotl.to(".letters-2", { opacity: 1, duration: 0.5 });
+        logotl.to(".letters-3", { opacity: 1, duration: 0.5 });
+        logotl.to(".letters-4", { opacity: 1, duration: 0.5 });
+        logotl.to(".letters-5", { opacity: 1, duration: 0.5 });
+        logotl.to(".letters-6", { opacity: 1, duration: 0.5 });
+        logotl.to(".spark", {opacity : 1, transformOrigin: 'center', duration:1, ease: "none",})
         const tl = gsap.timeline();
         tl.to("#line-one", { opacity: 1, duration: 0.5 }, 2);
         tl.to("#line-two", { opacity: 1, duration: 1 });
@@ -23,16 +31,25 @@ export default function Hero() {
             {/* Computer Layout */}
             <div className="hidden md:flex items-center justify-center">
 
-                    <div id="line-one" className="opacity-0"></div>
-                    <div id="line-two" className="opacity-0"></div>
+                    <div id="line-one" className="opacity-0 "></div>
+                    <div id="line-two" className="opacity-0 "></div>
 
-                <div className='logo opacity-0 flex text-7xl mt-40 items-center'>
-                    <p className='letters hover:text-9xl'>C</p>
-                    <p className='letters hover:text-9xl'>S</p>
-                    <p className='letters hover:text-9xl'>E</p>
-                    <p className='letters hover:text-9xl'>N</p>
-                    <p className='letters hover:text-9xl'>G</p>
-                    <p className='letters hover:text-9xl'>I</p>
+                <div className='logo  flex text-7xl mt-40 items-center ml-20'>
+                    <p className='letters-1 opacity-0 hover:text-9xl'>C</p>
+                    <p className='letters-2 opacity-0 hover:text-9xl'>S</p>
+                    <p className='letters-3 opacity-0 hover:text-9xl'>E</p>
+                    <p className='letters-4 opacity-0 hover:text-9xl'>N</p>
+                    <p className='letters-5 opacity-0 hover:text-9xl'>G</p>
+                    <p className='letters-6 opacity-0 hover:text-9xl'>I</p>
+                </div>
+                <div>
+                    <Image
+                        className='spark opacity-0'
+                        alt="spark"
+                        src="https://cdn3.iconfinder.com/data/icons/star-accents/32/star-set-expand-app-46-1024.png"
+                        width="100"
+                        height="100">
+                    </Image>
                 </div>
                 <svg width="300" height="300">
                     <path id="circlePath" d="M150 50 A 100 100 0 0 1 250 150 A 100 100 0 0 1 150 250 A 100 100 0 0 1 50 150 A 100 100 0 0 1 150 50" fill="none" stroke="transparent" />
